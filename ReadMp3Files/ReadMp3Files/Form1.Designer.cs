8faecfa574bf118163c9924aae9889837f6eec78
anushabubby <anusha26.mvg@gmail.com>
2017-07-17 22:35:07 -0500

    Add files via upload

---
+++ ReadMp3Files/ReadMp3Files/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ReadMp3Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(189, 133);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(172, 60);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please select the folder refactor the file details.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 358);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrowseButton);
+            this.Name = "Form1";
+            this.Text = "Update ID3 Tags";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ReadMp3Files/ReadMp3Files/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace ReadMp3Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog openDialog = new CommonOpenFileDialog();
+            openDialog.InitialDirectory = "C:\\Users";
+            openDialog.IsFolderPicker = true;
+            if (openDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string path = openDialog.FileName;
+                UpdateID3Tags(path);
+            }
+        }
+
+        public void UpdateID3Tags(string path)
+        {
+            string[] AllDirectories = Directory.GetDirectories(path);
+            if (AllDirectories.Length > 0)
+            {
+                foreach (string dirPath in AllDirectories)
+                {
+                    if (Directory.GetDirectories(dirPath).Length > 0)
+                    {
+                        UpdateID3Tags(dirPath);
+                    }
+                        Details directoryDetails = CheckDirectoryDetails(dirPath);
+                    if (directoryDetails.FilesArray.Length > 0)
+                    {
+                        RefactorFileDetails(directoryDetails.Path, directoryDetails.Name, directoryDetails.Year, directoryDetails.FilesArray);
+                    }
+                    else
+                    {
+                        //check if there are further directories.
+                    }
+                }
+            }
+            else if (AllDirectories.Length == 1)
+            {
+                Details directoryDetails = CheckDirectoryDetails(path);
+                RefactorFileDetails(directoryDetails.Path, directoryDetails.Name, directoryDetails.Year, directoryDetails.FilesArray);
+            }
+        }
+
+        public Details CheckDirectoryDetails(string path)
+        {
+            string dirName = null; uint dirYear = Convert.ToUInt32(DateTime.Now.Year.ToString());
+            Details newDirectoryDetails = new Details();
+
+            string directoryPath = path.ToString();
+            string directoryName = Path.GetFileName(directoryPath);
+            string[] FilesInDir = Directory.GetFiles(directoryPath, "*.mp3");
+
+            if (FilesInDir.Length > 0)
+            {
+                if (directoryName.Contains("("))
+                {
+                    dirName = directoryName.Split('(', ')')[0];
+                    dirYear = Convert.ToUInt32(directoryName.Split('(', ')')[1]);
+                }
+                newDirectoryDetails.Path = directoryPath;
+                newDirectoryDetails.Name = dirName != null ? dirName : directoryName;
+                newDirectoryDetails.Year = dirYear;
+                newDirectoryDetails.FilesArray = FilesInDir;
+            }
+            else
+            {
+                newDirectoryDetails.Path = directoryPath;
+                newDirectoryDetails.Name = directoryName;
+                newDirectoryDetails.Year = dirYear;
+                newDirectoryDetails.FilesArray = FilesInDir;
+            }
+            return newDirectoryDetails;
+    }
+
+        public void RefactorFileDetails(string dirPath, string dirName, uint dirYear, string[] FilesInDir)
+        {
+            foreach(string filePath in FilesInDir)
+            {
+                if (Path.GetExtension(filePath).ToLower() == ".mp3")
+                {
+                    TagLib.File file = TagLib.File.Create(filePath);
+                    file.Tag.Title = dirName;
+                    file.Tag.Album = dirName;
+                    string Genres = file.Tag.Genres.First();
+                    file.Tag.Year = dirYear;
+                    file.Save();
+                }
+            }
+
+        }
+    }
+}
